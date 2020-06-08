d5a1f91398d99c6b073cbb43813b9cc3a22ef425
Pandey,Anand <Anand.Pandey@cerner.com>
2020-06-08 12:29:29 +0530

    Backend Project

--- README.md
+++
@@ -1,1 +0,0 @@
-Calender Of Events
---
+++ src/main/java/com/calender/event/CalendarEventsApplication.java
@@ -0,0 +1,13 @@
+package com.calender.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarEventsApplication.class, args);
+	}
+
+}
